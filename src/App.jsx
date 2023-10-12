26d1570b4dcf4e76fc8a9bece3be9d9fbc21a202
JASPER HAEGEMAN <JASPER.HAEGEMAN@student.hogent.be>
2023-10-12 23:45:44 +0200

    stapvoorstap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "../src/components/Logo/Logo.jsx";
 import MotorDetail from "../src/components/motors/MotorDetails.jsx";
 import { MOTOR_DATA } from "../src/api/mock_data.js";
 import "./App.css";
@@ -6,15 +7,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="container">
-      {MOTOR_DATA.map((motor) => (
-        <MotorDetail
-          key={motor.id}
-          id={motor.id}
-          price={motor.price}
-          name={motor.name}
-          available={motor.available}
-        />
-      ))}
+      <Header />
+      <div className="motor-list">
+        {MOTOR_DATA.map((motor) => (
+          <MotorDetail
+            key={motor.id}
+            id={motor.id}
+            price={motor.price}
+            name={motor.name}
+            available={motor.available}
+          />
+        ))}
+      </div>
     </div>
   );
 }
