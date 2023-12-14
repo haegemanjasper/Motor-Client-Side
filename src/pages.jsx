8a11fb65d30a4961e59e98fbdb3db46b8b250f23
JASPER HAEGEMAN <JASPER.HAEGEMAN@student.hogent.be>
2023-12-14 11:59:29 +0100

    contact us

--- src/pages.jsx
+++ src/pages.jsx
@@ -1,5 +1,17 @@
-import React from "react";
-import { ChakraProvider, Box, Heading, Text, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  ChakraProvider,
+  Box,
+  Heading,
+  Text,
+  VStack,
+  Link,
+  Button,
+  Input,
+  Select,
+  Textarea,
+  FormLabel,
+} from "@chakra-ui/react";
 import theme from "./customTheme.js";
 import IntroText from "./components/Introduction";
 import SlideShow from "./components/Banner.jsx";
@@ -54,24 +66,135 @@ export const About = () => (
   </ChakraProvider>
 );
 
-export const Contact = () => (
-  <ChakraProvider theme={theme}>
-    <Box p={4}>
-      <Heading as="h1" mb={4}>
-        Contact Us
-      </Heading>
-      <VStack align="start" spacing={4}>
-        <Text fontSize="xl">
-          We'd love to hear from you! If you have any questions, suggestions, or
-          just want to say hello, feel free to reach out to us.
-        </Text>
-        <Text fontSize="xl">Email: info@motorcyclecommunity.com</Text>
-        <Text fontSize="xl">Phone: +1 (555) 123-4567</Text>
-      </VStack>
-    </Box>
-    <Footer />
-  </ChakraProvider>
-);
+export const Contact = () => {
+  const [formData, setFormData] = useState({
+    inquiryType: "",
+    collectionPoint: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    message: "",
+  });
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your logic to handle the form submission here
+    console.log("Form submitted:", formData);
+  };
+
+  // Handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box p={4}>
+        <Heading as="h1" mb={4}>
+          Contact Us
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <VStack align="start" spacing={2}>
+            <FormLabel mb={0}>
+              <Text fontWeight="bold">I have a question about...</Text>
+            </FormLabel>
+            <Select
+              name="inquiryType"
+              placeholder=""
+              value={formData.inquiryType}
+              onChange={handleChange}
+              isRequired
+            >
+              <option value="reservation">Reservation</option>
+              <option value="product">Motor</option>
+              <option value="contract">Contract</option>
+              <option value="location">Location</option>
+              <option value="general">General</option>
+              <option value="invoices">Invoices</option>
+            </Select>
+            <FormLabel mb={0}>
+              <Text fontWeight="bold">Select a collection point</Text>
+            </FormLabel>
+            <Select
+              name="collectionPoint"
+              placeholder="Select a collection point"
+              value={formData.collectionPoint}
+              onChange={handleChange}
+            >
+              <option value="pointA">Collection Point A</option>
+              <option value="pointB">Collection Point B</option>
+              <option value="pointC">Collection Point C</option>
+            </Select>
+            <FormLabel mb={0}>
+              <Text fontWeight="bold">First name</Text>
+            </FormLabel>
+            <Input
+              type="text"
+              name="firstName"
+              placeholder="First name"
+              value={formData.firstName}
+              onChange={handleChange}
+              isRequired
+            />
+            <FormLabel mb={0}>
+              <Text fontWeight="bold">Last name</Text>
+            </FormLabel>
+            <Input
+              type="text"
+              name="lastName"
+              placeholder="Last name"
+              value={formData.lastName}
+              onChange={handleChange}
+              isRequired
+            />
+            <FormLabel mb={0}>
+              <Text fontWeight="bold">Email address</Text>
+            </FormLabel>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email address"
+              value={formData.email}
+              onChange={handleChange}
+              isRequired
+            />
+            <FormLabel mb={0}>
+              <Text fontWeight="bold">Telephone number</Text>
+            </FormLabel>
+            <Input
+              type="tel"
+              name="phoneNumber"
+              placeholder="Telephone number"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              isRequired
+            />
+            <FormLabel mb={0}>
+              <Text fontWeight="bold">Message</Text>
+            </FormLabel>
+            <Textarea
+              name="message"
+              placeholder="Message"
+              value={formData.message}
+              onChange={handleChange}
+              isRequired
+            />
+            <Button type="submit" colorScheme="blue">
+              Submit
+            </Button>
+          </VStack>
+        </form>
+      </Box>
+      <Footer />
+    </ChakraProvider>
+  );
+};
 
 export const Models = () => (
   <div>
