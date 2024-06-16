9464c5de8c4638da8e8919b54cc15ba9737f05a4
JASPER HAEGEMAN <JASPER.HAEGEMAN@student.hogent.be>
2024-06-16 19:37:37 +0200

    basis imports voor auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "./context/auth-context.jsx";
+import theme from "./theme.js";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [{ index: true, element: <Home /> }],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <AuthProvider>
+            <ChakraProvider theme={theme}></ChakraProvider>
+        </AuthProvider>
     </React.StrictMode>
 );
