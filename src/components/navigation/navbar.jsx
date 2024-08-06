c0ff4f7e396f88d0802b16203d351384bb9cf823
JASPER HAEGEMAN <JASPER.HAEGEMAN@student.hogent.be>
2024-08-06 12:48:30 +0200

    klant blijft ingelogd na navigeren op site

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -3,62 +3,84 @@ import {
     Box,
     Flex,
     IconButton,
-    useDisclosure,
-    useColorMode,
-    useColorModeValue,
+    Stack,
     Divider,
     Container,
+    VStack,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { useDisclosure, useColorMode } from "@chakra-ui/react";
+import { useAuth } from "../../context/auth-context";
+import LogoComponent from "./Logo";
 import NavLinks from "./NavLinks";
 import AuthControls from "./AuthControls";
-import logo from "../../assets/logo site.png";
-import { useAuth } from "../../context/auth-context";
+
+const Links = [
+    { text: "Home", link: "/", roles: ["klant", "admin", "guest"] },
+    { text: "About Us", link: "/aboutus", roles: ["klant", "admin", "guest"] },
+    {
+        text: "Rent A Bike",
+        link: "/rentabike",
+        roles: ["klant", "admin", "guest"],
+    },
+];
 
 const Navbar = () => {
     const { colorMode, toggleColorMode } = useColorMode();
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { isAuthed, klant, logout } = useAuth();
-    const userRole = klant ? klant.role : "Klant";
+
+    // Zorg ervoor dat klant een geldige waarde heeft voordat je toegang krijgt tot klant.role
+    const userRole =
+        isAuthed && klant && klant.role ? klant.role.toLowerCase() : "guest";
+
+    // Filter de links op basis van de rol van de gebruiker
+    const filteredLinks = Links.filter((link) => link.roles.includes(userRole));
 
     return (
-        <Container maxW="1280px">
-            <Box>
-                <Flex
-                    h={16}
-                    alignItems={"center"}
-                    justifyContent={"space-between"}
-                >
-                    <IconButton
-                        size={"md"}
-                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                        aria-label={"Open Menu"}
-                        display={{ md: "none" }}
-                        onClick={isOpen ? onClose : onOpen}
-                    />
-                    <Flex alignItems={"center"}>
-                        <Box>
-                            <img
-                                src={logo}
-                                alt="Logo"
-                                height="50px"
-                                width="75px"
-                            />
-                        </Box>
-                        <NavLinks userRole={userRole} isOpen={false} />
+        <>
+            <Container maxW="1280px">
+                <Box>
+                    <Flex
+                        h={16}
+                        alignItems={"center"}
+                        justifyContent={"space-between"}
+                    >
+                        <IconButton
+                            size={"md"}
+                            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                            aria-label={"Open Menu"}
+                            display={{ md: "none" }}
+                            onClick={isOpen ? onClose : onOpen}
+                        />
+                        <Flex alignItems={"center"} spacing={8}>
+                            <LogoComponent />
+                            <NavLinks filteredLinks={filteredLinks} />
+                        </Flex>
+                        <AuthControls
+                            isAuthed={isAuthed}
+                            logout={logout}
+                            toggleColorMode={toggleColorMode}
+                            userName={klant?.naam}
+                        />
                     </Flex>
-                    <AuthControls
-                        isAuthed={isAuthed}
-                        klant={klant}
-                        logout={logout}
-                        toggleColorMode={toggleColorMode}
-                        colorMode={colorMode}
-                    />
-                </Flex>
-                {isOpen && <NavLinks userRole={userRole} isOpen={true} />}
+                    {isOpen ? (
+                        <Box pb={4} display={{ md: "none" }}>
+                            <Stack as={"nav"} spacing={4}>
+                                <NavLinks filteredLinks={filteredLinks} />
+                                <AuthControls
+                                    isAuthed={isAuthed}
+                                    logout={logout}
+                                    toggleColorMode={toggleColorMode}
+                                    userName={klant?.naam}
+                                />
+                            </Stack>
+                        </Box>
+                    ) : null}
+                </Box>
                 <Divider />
-            </Box>
-        </Container>
+            </Container>
+        </>
     );
 };
 
