a723fbc944cf37f9225f362370f431967ed994ff
JASPER HAEGEMAN <JASPER.HAEGEMAN@student.hogent.be>
2024-08-08 19:18:41 +0200

    about us page managed

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -1,33 +1,76 @@
 import React from "react";
-import { Box, Heading, Text, Container } from "@chakra-ui/react";
+import { Box, Heading, Text, Container, Stack } from "@chakra-ui/react";
 
 export default function AboutUsPage() {
     return (
-        <Container maxW="container.md" py={10}>
-            <Box textAlign="center">
-                <Heading as="h1" size="xl" mb={4}>
-                    About Us
-                </Heading>
-                <Text fontSize="lg" mb={6}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus imperdiet, nulla nec cursus dapibus, urna metus
-                    iaculis mauris, ut laoreet odio leo eu ligula. Donec sed
-                    fermentum magna. Quisque vitae varius libero. Nulla
-                    facilisi. Curabitur in interdum felis. Vestibulum ac nibh ac
-                    nunc tincidunt cursus non sit amet metus. Pellentesque ac
-                    neque eget erat fermentum fringilla.
-                </Text>
-                <Text fontSize="lg">
-                    Morbi laoreet mauris ut tortor sodales, a interdum sapien
-                    tincidunt. Mauris vehicula, nisi ac vehicula consequat,
-                    risus quam efficitur risus, eu dapibus urna justo nec lorem.
-                    Vestibulum varius vitae urna eget iaculis. Duis sit amet
-                    fermentum ex. Fusce scelerisque orci sed neque tempor, nec
-                    fermentum odio pretium. Aenean auctor venenatis urna, at
-                    consequat nunc eleifend ut. Donec hendrerit nisi at ligula
-                    cursus, a cursus ligula lacinia.
-                </Text>
-            </Box>
-        </Container>
+        <Box bg="gray.100" py={10}>
+            <Container maxW="container.md">
+                <Box textAlign="center" mb={10}>
+                    <Heading as="h1" size="2xl" mb={6}>
+                        About Us
+                    </Heading>
+                    <Text fontSize="lg" color="gray.700">
+                        We are a passionate team dedicated to providing the best
+                        products and services to our customers. Our mission is
+                        to deliver quality and excellence in everything we do.
+                    </Text>
+                </Box>
+                <Stack spacing={8} align="center">
+                    <Box
+                        p={6}
+                        bg="white"
+                        shadow="md"
+                        borderRadius="md"
+                        textAlign="center"
+                    >
+                        <Heading as="h2" size="lg" mb={4}>
+                            Our Vision
+                        </Heading>
+                        <Text fontSize="md" color="gray.600">
+                            Our vision is to be the leading provider of
+                            innovative solutions that enrich the lives of our
+                            customers. We strive to continuously improve and
+                            stay ahead of the curve in our industry.
+                        </Text>
+                    </Box>
+                    <Box
+                        p={6}
+                        bg="white"
+                        shadow="md"
+                        borderRadius="md"
+                        textAlign="center"
+                    >
+                        <Heading as="h2" size="lg" mb={4}>
+                            Our Values
+                        </Heading>
+                        <Text fontSize="md" color="gray.600">
+                            We believe in integrity, commitment, and excellence.
+                            Our team is dedicated to upholding these values in
+                            every aspect of our work, ensuring that we provide
+                            the highest level of service and satisfaction to our
+                            clients.
+                        </Text>
+                    </Box>
+                    <Box
+                        p={6}
+                        bg="white"
+                        shadow="md"
+                        borderRadius="md"
+                        textAlign="center"
+                    >
+                        <Heading as="h2" size="lg" mb={4}>
+                            Meet the Team
+                        </Heading>
+                        <Text fontSize="md" color="gray.600">
+                            Our team is made up of skilled professionals who
+                            bring a wealth of experience and passion to their
+                            work. Get to know the individuals behind our success
+                            and learn about their unique contributions to our
+                            mission.
+                        </Text>
+                    </Box>
+                </Stack>
+            </Container>
+        </Box>
     );
 }
